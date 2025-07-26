822a5bde184b4de326ade292daa0292ee3811bb8
Phạm Văn Phụng <vanphung0198@gmail.com>
2025-07-26 14:58:01 +0700

    innit commit

---
+++ wedding-invite/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>Trân trọng kính mời</h1>
+      <p>Đến dự lễ thành hôn của chúng tôi</p>
+      <h2>... ❤️ ...</h2>
+      <p>Vào lúc 17h ngày 20/10/2025<br />Tại Nhà hàng iPhung</p>
+    </div>
+  );
+}
+
+export default App;
